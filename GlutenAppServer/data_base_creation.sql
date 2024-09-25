ae6d00cfdc5556b0104279598791ed0b28fd78c6
User <User@LAB2-2>
2024-09-25 09:13:52 +0300

    data base finale probs

--- GlutenAppServer/data_base_creation.sql
+++ GlutenAppServer/data_base_creation.sql
@@ -13,6 +13,18 @@ Go
 Use GlutenFree_DB
 Go
 
+--יצירת טבלת סוג אוכל
+CREATE TABLE TypeFood(
+TypeFoodID INT PRIMARY KEY IDENTITY,   --Primary Key
+TypeFoodName NVARCHAR (30)             --Types of food categories
+);
+
+--יצירת טבלת סוג משתמשים
+CREATE TABLE TypeUserID(
+TypeID INT PRIMARY KEY IDENTITY,       --Primary Key
+TypeName NVARCHAR (20)                 --Type Name
+);
+
 --יצירת טבלת משתמשים
 CREATE TABLE Users(
 UserID INT PRIMARY KEY IDENTITY,         --Primary key
@@ -20,12 +32,6 @@ UserName NVARCHAR (20),                  --The Username
 UserPass NVARCHAR (20),                  --Password
 TypeID INT,
 FOREIGN KEY (TypeID) REFERENCES TypeUserId(TypeID)  --Foreign key to show user types
-);
-
---יצירת טבלת סוג משתמשים
-CREATE TABLE TypeUserID(
-TypeID INT PRIMARY KEY IDENTITY,       --Primary Key
-TypeName NVARCHAR (20)                 --Type Name
 );
 
 --יצירת טבלת מסעדות
@@ -38,18 +44,15 @@ TypeFoodID INT,
 FOREIGN KEY (TypeFoodID) REFERENCES TypeFood(TypeFoodID) --Foreign key from type food to show the type
 );
 
---יצירת טבלת סוג אוכל
-CREATE TABLE TypeFood(
-TypeFoodID INT PRIMARY KEY IDENTITY,   --Primary Key
-TypeFoodName NVARCHAR (30)             --Types of food categories
-);
 
 --יצירת טבלת ביקורות
 CREATE TABLE Critics(
 CriticID INT PRIMARY KEY IDENTITY,  --Primary Key
 CriticText NVARCHAR (1000),         --The Text 
 UserID INT,
-FOREIGN KEY (UserID) REFERENCES Users(UserID) --Foreign key - the writer 
+FOREIGN KEY (UserID) REFERENCES Users(UserID), --Foreign key - the writer 
+RestID INT,
+FOREIGN KEY (RestID) REFERENCES Restaurants(RestID) --Foreign key - the said restaurant
 );
 
 --יצירת טבלת מתכונים
@@ -91,4 +94,15 @@ INSERT INTO TypeFood VALUES ('Asian')
 INSERT INTO TypeFood VALUES ('Mexican')
 INSERT INTO TypeFood VALUES ('BBQ')
 INSERT INTO TypeFood VALUES ('French')
+
+--הכנסת דמה לכל הטבלאות
+INSERT INTO Users VALUES ('Gal', 'GalTheBest123', 1)        --gal the first user. userid = 1, typeid = 1
+INSERT INTO Users VALUES ('Shahar', 'ShaharTheBest123', 3)  --shahar the first restaurant manager. userid = 2, typeid = 3
+INSERT INTO Restaurants VALUES ('Ramon School', 2, 1)       --shahar's restaurant - restid =1, typefood = italian
+INSERT INTO Recipes VALUES ('lalalala', 1)                  --gal's recipe
+INSERT INTO Critics VALUES ('I love this restaurant', 1,1)  --gal's critic - about shahar's restaurant restid = 1, userid = 1 
+INSERT INTO Information VALUES ('Gluten is tasty - Shahar Shalgi')  --shahar's information - will be deleted
+
+SELECT * FROM Users
+
 
